95eb8a82826e5aeed3f3d7f5776d334dc9a9807f
Grant Warren <gwarren1715@gmail.com>
2015-04-29 05:31:39 +0000

    Finished user signupform complete with tests

---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,32 @@
+require 'test_helper'
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+  require 'test_helper'
+
+  test "invalid signup information" do
+    get signup_path
+    assert_no_difference 'User.count' do
+    before_count = User.count
+      post users_path, user: { name:"", 
+                              email: "invalid@invalid.com",
+                              password: "foo", 
+                              password_confirmation: "bar"}
+    end
+    assert_template "users/new"
+   
+    
+  end
+    test "valid signup information" do
+    get signup_path
+    assert_no_difference 'User.count' do
+    before_count = User.count,1 do
+      post_via_redirect users_path, user: { name:"Grant", 
+                              email: "Grant@grant.com",
+                              password: "imgrant", 
+                              password_confirmation: "imgrant"}
+    end
+    #assert_template "users/new"
+   
+    
+  end
+end
